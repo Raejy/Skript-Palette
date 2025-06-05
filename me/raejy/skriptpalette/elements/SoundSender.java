fdd1ec6cb10a83943a5d84804d1730d2aa6e3151
Raejy <rainonmc@gmail.com>
2025-06-05 13:55:26 +0200

    Update SoundSender.java

--- me/raejy/skriptpalette/elements/SoundSender.java
+++ me/raejy/skriptpalette/elements/SoundSender.java
@@ -1,1 +1,10 @@
-player.playSound(player.getLocation(), "minecraft:custom.sound", SoundCategory.MASTER, 1f, 1f);
+private final Map<UUID, String> userSounds = new HashMap<>();
+
+public void setUserSound(UUID userId, String soundId) {
+    userSounds.put(userId, soundId);
+}
+
+public void playCustomSound(Player player) {
+    String sound = userSounds.getOrDefault(player.getUniqueId(), "minecraft:entity.player.levelup");
+    player.playSound(player.getLocation(), sound, 1f, 1f);
+}
