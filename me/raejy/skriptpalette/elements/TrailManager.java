5aaeb0326d0ecd9ac9a2f7b985e3cce9f69248b5
Raejy <rainonmc@gmail.com>
2025-06-05 13:56:01 +0200

    Update TrailManager.java

--- me/raejy/skriptpalette/elements/TrailManager.java
+++ me/raejy/skriptpalette/elements/TrailManager.java
@@ -1,16 +1,8 @@
-Map<UUID, BukkitTask> trails = new HashMap<>();
+private final Map<UUID, TrailSettings> trailPrefs = new HashMap<>();
 
-public void startTrail(Player player) {
-    BukkitTask task = Bukkit.getScheduler().runTaskTimer(plugin, () -> {
-        player.getWorld().spawnParticle(Particle.REDSTONE,
-            player.getLocation().add(0, 1, 0),
-            1,
-            new Particle.DustOptions(Color.fromRGB(255, 0, 255), 1));
-    }, 0L, 5L);
-    trails.put(player.getUniqueId(), task);
+public record TrailSettings(String type, Color color) {}
+
+public void setTrailSettings(Player player, String type, Color color) {
+    trailPrefs.put(player.getUniqueId(), new TrailSettings(type, color));
 }
 
-public void stopTrail(Player player) {
-    BukkitTask task = trails.remove(player.getUniqueId());
-    if (task != null) task.cancel();
-}
